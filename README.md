102119a9977e1dbe9f396da2ea17b5d1685b6dd6
JQ <jqocon@gmail.com>
2020-02-09 17:10:54 +0000

    fix: no husky by default

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # setup-gh-badges
 gh-badges made simple
+
+## Usage
+
+- `yarn add -D setup-gh-badges`
+
+Will append the following to your current npm scripts under `package.json`:
+
+```
+"scripts": {
+    // ...your scripts,
+    "badges:win": "./scripts/generate_badges_win.sh"
+},
+```
+
+> NOTE: Feel free to send a PR for a Mac Bash version or even better a cross-platform JS one :-)
+
+## Recommended if you don't have a CI server
+
+- `yarn add -D husky`
+
+- Add the following hook to your `package.json`:
+
+```
+"husky": {
+    "hooks": {
+        "pre-push": "./scripts/generate_badges_win.sh"
+    }
+}
+```
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 const path = require('path');
 const fs = require('fs');
 const jsonfile = require('jsonfile');
@@ -10,33 +8,27 @@ const rootPkgPath = path.join(parentModuleBase, 'package.json');
 let rootPkg = null;
 
 try {
-    console.log({ rootPkgPath });
     rootPkg = JSON.parse(fs.readFileSync(rootPkgPath).toString());
-    console.log({ rootPkg });
 
     let { scripts } = rootPkg;
     scripts = {
         ...scripts,
-        "badges:win": "./scripts/generate_badges_win.sh"
+        "badges:win": "scripts/generate_badges_win.sh"
     };
 
-    const husky = {
-        hooks: {
-            "pre-push": "./scripts/generate_badges_win.sh"
-        }
-    }
-
+    // Update parent module's package.json
     jsonfile.writeFileSync(rootPkgPath, {
         ...rootPkg,
-        scripts: { ...scripts },
-        husky
+        scripts: { ...scripts }
     });
 
+    // Copy the bash script
     const scriptDestDir = path.join(parentModuleBase, 'scripts');
     !fs.existsSync(scriptDestDir) && fs.mkdirSync(scriptDestDir);
     const scriptDestPath = path.join(scriptDestDir, 'generate_badges_win.sh');
     fs.copyFileSync('generate_badges_win.sh', scriptDestPath);
 
+    // Generate folder to hold badges
     const ciDestDir = path.join(parentModuleBase, '.ci');
     !fs.existsSync(ciDestDir) && fs.mkdirSync(ciDestDir);
     
