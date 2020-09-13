583a517eb5c9c2919cdaad169eedc2f5e4a8ef4c
JQ <jqocon@gmail.com>
2020-09-13 22:29:35 +0100

    v2.1.1

--- cli.js
+++ cli.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
 import syncBadges from './index.js'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { readFileSync } from 'fs'
 
 const [,arg1, arg2] = process.argv
 const argsStr = [arg1, arg2].join(' ')
 if (/\s-v\s*$/.test(argsStr)) {
-    const { name, version } = require('./package')
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = dirname(__filename);
+    const rawPkg = readFileSync(resolve(__dirname, 'package.json'))
+    const { name, version } = JSON.parse(rawPkg)
     console.log(`${name} v${version}    ✔️`)
     process.exit()
 }
