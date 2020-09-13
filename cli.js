79d48a3224146bdc998103834551bbde1a6254ff
JQ <jqocon@gmail.com>
2020-09-13 22:11:54 +0100

    v2.1.0

--- cli.js
+++ cli.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-const syncBadges = require('./index');
+import syncBadges from './index.js'
 
 const [,arg1, arg2] = process.argv
 const argsStr = [arg1, arg2].join(' ')
 if (/\s-v\s*$/.test(argsStr)) {
-    const { name, version } = require('./package');
-    console.log(`${name} v${version}    ✔️`);
-    process.exit();
+    const { name, version } = require('./package')
+    console.log(`${name} v${version}    ✔️`)
+    process.exit()
 }
 
-syncBadges();
+syncBadges()
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const { resolve, join } = require('path');
-const { existsSync, mkdirSync, writeFileSync, readFileSync } = require('fs');
-const { makeBadge } = require('badge-maker');
+import { resolve, join } from 'path'
+import { existsSync, mkdirSync, writeFileSync, readFileSync } from 'fs'
+import badgeMaker from 'badge-maker'
+
+const { makeBadge } = badgeMaker
 
 const createBadge = ({
     name, label, message, color = 'green', style = 'flat'
 }) => {
     // Generate folder to hold badges
-    const ciDestDir = resolve(process.cwd(), '.ci_badges');
-    !existsSync(ciDestDir) && mkdirSync(ciDestDir);
+    const ciDestDir = resolve(process.cwd(), '.ci_badges')
+    !existsSync(ciDestDir) && mkdirSync(ciDestDir)
 
     const badgeConfig = {
         label,
@@ -17,19 +19,19 @@ const createBadge = ({
     }
     
     const svg = makeBadge(badgeConfig)
-    writeFileSync(join(ciDestDir, name), svg);
+    writeFileSync(join(ciDestDir, name), svg)
 }
 
 const getNumColor = (num) => {
- if (num < 1) return 'green';
- if (num > 10) return 'orange';
- if (num > 5) return 'yellow';
+ if (num < 1) return 'green'
+ if (num > 10) return 'orange'
+ if (num > 5) return 'yellow'
 }
 
 const syncBadges = async() => {
-    const rootPkgPath = resolve(process.cwd(), 'package.json');
-    const rootPkg = JSON.parse(readFileSync(rootPkgPath).toString());
-    const { name = '', version = '', dependencies = {}, devDependencies = {} } = rootPkg;
+    const rootPkgPath = resolve(process.cwd(), 'package.json')
+    const rootPkg = JSON.parse(readFileSync(rootPkgPath).toString())
+    const { name = '', version = '', dependencies = {}, devDependencies = {} } = rootPkg
 
     // Get version
     createBadge({
@@ -39,28 +41,28 @@ const syncBadges = async() => {
     });
 
     // Get dependencies
-    const numDependencies = Object.keys(dependencies).length;
+    const numDependencies = Object.keys(dependencies).length
     createBadge({
         name: 'npm-dependencies-badge.svg', 
         label: 'dependencies',
         message: numDependencies.toString(),
         color: getNumColor(numDependencies)
-    });
+    })
 
     // Get devDependencies
-    const numDevDependencies = Object.keys(devDependencies).length;
+    const numDevDependencies = Object.keys(devDependencies).length
     createBadge({
         name: 'npm-devdependencies-badge.svg', 
         label: 'devDependencies',
         message: numDevDependencies.toString(),
         color: getNumColor(numDevDependencies)
-    });
+    })
 
     console.log(`\n\n✔️   ${name} v${version}: Badges updated successfully!
     
     # version ${version}
     # dependencies ${numDependencies}
-    # devDependencies ${numDevDependencies}\n\n`);
-};
+    # devDependencies ${numDevDependencies}\n\n`)
+}
 
-module.exports = syncBadges;
+export default syncBadges
